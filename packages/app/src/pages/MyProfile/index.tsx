eac2f9c4aaaf9caec767a873cf189cb2d5865000
Andrei Radu <70237539+aandreiradu@users.noreply.github.com>
2023-04-10 22:17:14 +0300

    Hide default content from /my-profile if the user is not logged in (#1)

--- packages/app/src/pages/MyProfile/index.tsx
+++ packages/app/src/pages/MyProfile/index.tsx
@@ -82,39 +82,40 @@ const MyProfile = () => {
         />
       )}
 
-      {isLoading ? (
-        <div className="w-full h-screen flex items-center justify-center">
-          <PulseLoader />
-        </div>
-      ) : (
-        <div className="flex flex-col w-full mx-auto max-w-7xl px-2 py-2  sm:px-10 lg:px-8 lg:py-5">
-          <WelcomeBackHeader />
-
-          <div className="mt-4 w-full p-4 rounded-md bg-gray-200">
-            <div className="my-4 w-full flex flex-col items-end justify-center ">
-              <p
-                className="mb-2 relative w-fit px-3 text-base text-defaultBlack md:text-lg uppercase  
-                      after:absolute after:bottom-0 after:left-0  after:h-[2px] after:bg-red-500
-                      after:w-full after:animate-growingWidth"
-              >
-                Account Info
-              </p>
-              <span className="text-base font-bold">Email: {email}</span>
-              <span className="text-base font-bold">Username: {username}</span>
-              <span className="text-base font-bold">Full Name: {fullName}</span>
-            </div>
-
-            <h2 className="text-xl uppercase font-bold">Your Tasks:</h2>
-            <ul className="grid grid-cols-2 gap-x-4 gap-y-4 mt-2 ">
-              {userTasks?.length === 0 ? (
-                <p>No tasks registered</p>
-              ) : (
-                userTasks?.map((task) => <TaskItem key={task.id} name={task.todo} id={task.id} status={task.completed} />)
-              )}
-            </ul>
+      {isLoggedIn &&
+        (isLoading ? (
+          <div className="w-full h-screen flex items-center justify-center">
+            <PulseLoader />
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="flex flex-col w-full mx-auto max-w-7xl px-2 py-2  sm:px-10 lg:px-8 lg:py-5">
+            <WelcomeBackHeader />
+
+            <div className="mt-4 w-full p-4 rounded-md bg-gray-200">
+              <div className="my-4 w-full flex flex-col items-end justify-center ">
+                <p
+                  className="mb-2 relative w-fit px-3 text-base text-defaultBlack md:text-lg uppercase  
+                          after:absolute after:bottom-0 after:left-0  after:h-[2px] after:bg-red-500
+                          after:w-full after:animate-growingWidth"
+                >
+                  Account Info
+                </p>
+                <span className="text-base font-bold">Email: {email}</span>
+                <span className="text-base font-bold">Username: {username}</span>
+                <span className="text-base font-bold">Full Name: {fullName}</span>
+              </div>
+
+              <h2 className="text-xl uppercase font-bold">Your Tasks:</h2>
+              <ul className="grid grid-cols-2 gap-x-4 gap-y-4 mt-2 ">
+                {userTasks?.length === 0 ? (
+                  <p>No tasks registered</p>
+                ) : (
+                  userTasks?.map((task) => <TaskItem key={task.id} name={task.todo} id={task.id} status={task.completed} />)
+                )}
+              </ul>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
